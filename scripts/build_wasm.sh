32538096c25fa558d9cb870124a9ffa714bb4886
Thierry Berger <contact@thierryberger.com>
2023-04-07 18:34:54 +0200

    fix script to build

--- scripts/build_wasm.sh
+++ scripts/build_wasm.sh
@@ -7,5 +7,5 @@ BINARY_NAME=Sidle_Ffect
 
 cargo build --bin $BINARY_NAME --profile profile --target wasm32-unknown-unknown
 cp -r wasm/* generated_wasm/
-wasm-bindgen --no-typescript --out-name bevy_game --out-dir generated_wasm --target web ./target/wasm32-unknown-unknown/wasm-release/$BINARY_NAME.wasm
+wasm-bindgen --no-typescript --out-name bevy_game --out-dir generated_wasm --target web ./target/wasm32-unknown-unknown/profile/$BINARY_NAME.wasm
 cp -r assets generated_wasm/
